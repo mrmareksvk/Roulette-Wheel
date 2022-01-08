cb276fb464a2348334237ef3fc982f737686902b
mrmareksvk <marek.visnovec@gmail.com>
2022-01-08 02:07:58 +0100

    Outer Ring - working
    Simulated on wokwi, preparation for inner ring sync

--- Embedded/RouletteSegmentDEMO.ino
+++ Embedded/RouletteSegmentDEMO.ino
@@ -1,7 +1,7 @@
 /*
    Embedded program Arduino Serial and Light Control
    For Project #2 Roulette wheel - KEA PTI 5th. semester
-*/
+ */
 
 // LIBRARIES, IMPORTS
 #include <Adafruit_NeoPixel.h>
@@ -10,155 +10,180 @@
 // DEFINITIONS
 #define PIN_OUT 5
 #define PIN_IN 6
-#define LED_OUT 195
+#define LED_OUT 194
 #define LED_IN 49
 
+#define DEBUG false // <-printouts
+
 // VARIABLES and CONSTANTS
-const byte ser_max_size = 200;
-char ser_buffer[ser_max_size]; // change for longer commands
+const byte ser_max_size = 100; // change for longer commands
+char ser_buffer[ser_max_size];
 byte msg_size = 0;
 bool msg_complete = false;
 
 byte segment_start = 0;
-byte segment_stop;
 byte segment_step;
+byte segment_runs;
+byte segment_stop;
 
 // INITIALISATIONS
 Adafruit_NeoPixel strip_out = Adafruit_NeoPixel(LED_OUT, PIN_OUT, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel strip_in = Adafruit_NeoPixel(LED_IN, PIN_IN, NEO_GRB + NEO_KHZ800);
 
-StaticJsonDocument<ser_max_size> com_doc; // stored in STACK (DynamicJsonDocument in HEAP)
+StaticJsonDocument<ser_max_size> com_doc; // stored in STACK
 
 // FUNCTIONS
 void clearStrip(int strip) { // 1 == outer strip | 2 == inner strip || 12 == both
-  if (strip == 1) {
-    for (int LED = 0; LED < LED_OUT; LED++) {
-      strip_out.setPixelColor(LED, 0);
-    }
-    strip_out.show();
-  } else if (strip == 2) {
-    for (int LED = 0; LED < LED_IN; LED++) {
-      strip_in.setPixelColor(LED, 0);
-    }
-    strip_in.show();
-  } else if (strip == 12) {
-    for (int LED = 0; LED < LED_OUT; LED++) {
-      strip_out.setPixelColor(LED, 0);
-    }
-    strip_out.show();
-    for (int LED = 0; LED < LED_IN; LED++) {
-      strip_in.setPixelColor(LED, 0);
-    }
-    strip_in.show();
-  }
+        if (strip == 1) {
+                for (int LED = 0; LED < LED_OUT; LED++) {
+                        strip_out.setPixelColor(LED, 0);
+                }
+                strip_out.show();
+        } else if (strip == 2) {
+                for (int LED = 0; LED < LED_IN; LED++) {
+                        strip_in.setPixelColor(LED, 0);
+                }
+                strip_in.show();
+        } else if (strip == 12) {
+                for (int LED = 0; LED < LED_OUT; LED++) {
+                        strip_out.setPixelColor(LED, 0);
+                }
+                strip_out.show();
+                for (int LED = 0; LED < LED_IN; LED++) {
+                        strip_in.setPixelColor(LED, 0);
+                }
+                strip_in.show();
+        }
 }
 
 void outSegmentRoulette(void) {
-  int s_LEDstart = LED_OUT;
-  int seg = 0;
-  int runs = 3000 / segment_step;
-  int segment_run = 0;
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 
-  int run_color1 = random(255);
-  int run_color2 = random(255);
-  int run_color3 = random(255);
+        int seg = 0;
+        int LEDoutRange = LED_OUT;
 
-  clearStrip(1);
-  //{5 - 12    115 , 7 = 108 , d 12 = 9, last end 12}
-  // 11 2 , 40 = 75, 74 / 12 = 6.1, end 2 .
-  for (int run = 0; run < runs; run++) {
-    if (segment_run == 0) {
-      seg = segment_start;
-    } else {
-      seg = 0;
-    }
-    for (int segment = seg; segment < 12; segment++) {
-      if (segment == 0) {
-        s_LEDstart = LED_OUT;
-      } else {
-        s_LEDstart = LED_OUT - (segment * 16); // 16 ~= # of segment LEDs
-      }
-      // if segment run = maximum a segment + 1 == segment_end, break - nechod do posledneho foru
-      if (segment_run == (runs - 1)) {
-        // Serial.println("break");
-        return;
-      }
-      for (int LED = s_LEDstart; LED > s_LEDstart - 16; LED--) {
-        // Serial.println(LED); // DEBUG
-        strip_out.setPixelColor(LED, strip_out.Color(run_color1, run_color2, run_color3));
-        if (LED == s_LEDstart - 15) {
-          // do this only in last iteration (substraction error)
-          strip_out.setPixelColor(LED - 1, strip_out.Color(run_color1, run_color2, run_color3));
-          strip_out.setPixelColor(LED - 2, strip_out.Color(run_color1, run_color2, run_color3));
-          strip_out.setPixelColor(LED - 3, strip_out.Color(run_color1, run_color2, run_color3));
+        int run_color1 = random(255);
+        int run_color2 = random(255);
+        int run_color3 = random(255);
+
+        clearStrip(12);
+
+        for (int run = 1; run <= segment_runs + 1; run++) {
+                if (run == 1) {
+                        seg = segment_start - 1;
+                        // Serial.println(seg+1); // DEBUG
+                }
+                // loop done - reset position, change color
+                if (seg >= 12 && run != 1) {
+                        seg = 0;
+                        run_color1 = random(255);
+                        run_color2 = random(255);
+                        run_color3 = random(255);
+                }
+
+                if (seg == 0) {
+                        clearStrip(12);
+                        LEDoutRange = LED_OUT;
+                } else {
+                        LEDoutRange = LED_OUT - (seg * 16);
+                }
+
+                for (int LED = LEDoutRange; LED > LEDoutRange -16; LED--) {
+                        // Clear old pixels
+                        if (LED > LED_OUT - 16) {
+                                // PASS - NOCLEAR}
+                        } else {
+                                strip_out.setPixelColor(LED+16, 0);
+                        }
+                        strip_out.setPixelColor(LED, strip_out.Color(run_color1, run_color2, run_color3));
+                        if (LED == LEDoutRange - 15 && seg == 11) {
+                                strip_out.setPixelColor(LED - 1, strip_out.Color(run_color1, run_color2, run_color3));
+                                strip_out.setPixelColor(LED - 2, strip_out.Color(run_color1, run_color2, run_color3));
+                                strip_out.setPixelColor(LED - 3, strip_out.Color(run_color1, run_color2, run_color3));
+                        }
+                }
+                strip_out.show();
+                seg++;
+                if (DEBUG) {
+                        Serial.print("run: ");
+                        Serial.println(run);
+                        Serial.print("segReal: "); Serial.println(seg);
+                }
+                // if (run == segment_runs + 1) Serial.println(seg); // DEBUG
+
+                delay(segment_step);
         }
-      }
-      strip_out.show();
-      delay(segment_step);
-      segment_run++;
-    }
-  }
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); // DEBUG
 }
 
-void readSerial(void) { // TODO - use native ArduinoJson reader
-  while (Serial.available()) {
-    char c = Serial.read();
-    if (c == '\n') { //  end terminator: LF (line feed "\n") / null ("\0")
-      msg_complete = true;
-      msg_size++;
-      break;
-    }
-    else {
-      if (msg_size < ser_max_size - 1) {
-        ser_buffer[msg_size] = c;
-        msg_size++;
-      }
-      else {
-        // if overflow
-      }
-    }
-  }
+void readSerial(void) {
+        while (Serial.available()) {
+                char c = Serial.read();
+                if (c == '\n') { //  end terminator: LF (line feed "\n")
+                        msg_complete = true;
+                        msg_size++;
+                        break;
+                }
+                else {
+                        if (msg_size < ser_max_size - 1) {
+                                ser_buffer[msg_size] = c;
+                                msg_size++;
+                        }
+                        else {
+                                // if overflow
+                                if (DEBUG) Serial.println("BUFF OVERFLOW");
+                        }
+                }
+        }
 }
 
 void handleMessage(void) {
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 
-  // Serial.println(ser_buffer);
-  DeserializationError err = deserializeJson(com_doc, ser_buffer);
-  if (err) {
-    // Serial.println("deserialization error");
-    return;
-  } else {
-    segment_start = com_doc["att"]["start"];
-    segment_stop = com_doc["att"]["stop"];
-    segment_step = com_doc["att"]["t"];
-    outSegmentRoulette();
-  }
+        if (DEBUG) Serial.println(ser_buffer);
+
+        DeserializationError err = deserializeJson(com_doc, ser_buffer);
+        if (err) {
+                if (DEBUG) Serial.println(">deserialization error");
+
+                msg_size = 0;
+                msg_complete = false;
+                return;
+        } else {
+                if (DEBUG) Serial.println(">cmd start");
+
+                segment_start = com_doc["at"]["strt"];
+                segment_step = com_doc["at"]["t"];
+                segment_runs = com_doc["at"]["r"];
+                segment_stop = com_doc["at"]["stp"];
+
+                outSegmentRoulette();
+
+                msg_size = 0;
+                msg_complete = false;
+        }
 }
 
 
 void setup() {
-  Serial.begin(115200); // same speed as Raspberry
-  pinMode(LED_BUILTIN, OUTPUT);
+        Serial.begin(115200); // same speed as on Raspberry
+        pinMode(LED_BUILTIN, OUTPUT);
 
-  strip_out.begin();
-  strip_in.begin();
+        strip_out.begin();
+        strip_in.begin();
 
-  clearStrip(12);
+        clearStrip(12);
 
-  randomSeed(analogRead(0));
+        randomSeed(analogRead(0));
 }
 
 
 void loop() {
-  if (Serial.available()) {
-    readSerial();
-  }
+        if (Serial.available()) {
+                readSerial();
+        }
 
-  if (msg_complete) {
-    handleMessage();
-    msg_complete = false;
-    msg_size = 0;
-  }
-
+        if (msg_complete) {
+                handleMessage();
+        }
 }
